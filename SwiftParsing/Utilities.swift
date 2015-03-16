2bcdbf8dc72af9af3071e0be4cad5aaf550d2b3e
Jonathan Wight <jwight@mac.com>
2015-03-16 15:50:44 -0700

    Removed CLI - filled out a unit test.

--- SwiftParsing/Utilities.swift
+++ SwiftParsing/Utilities.swift
@@ -22,3 +22,31 @@ extension Double {
     }
 }
 
+func getChildren(element:Element) -> [Element]? {
+    if let element = element as? ContainerElement {
+        return element.subelements
+    }
+    return nil
+}
+
+func dump <T> (element:T, depth:Int = 0, children:T -> [T]?) {
+    let description = reflect(element).summary
+//    let description = toString(element)
+
+    let spaces = reduce(0..<depth, "") {
+        (U:String, index:Int) -> String in
+        return U + "  "
+        }
+    println("\(spaces)\(description)")
+    if let childElements = children(element) {
+        for child in childElements {
+            dump(child, depth:depth + 1, children)
+        }
+    }
+}
+
+extension Element {
+    func dump() {
+        SwiftParsing.dump(self, depth: 0, getChildren)
+    }
+}
--- SwiftParsingTests/SwiftParsingTests.swift
+++ SwiftParsingTests/SwiftParsingTests.swift
@@ -8,29 +8,37 @@
 
 import Cocoa
 import XCTest
+import SwiftParsing
+
+let COMMA = Literal(",")
+let OPT_COMMA = zeroOrOne(COMMA).makeStripped()
+let LPAREN = Literal("(").makeStripped()
+let RPAREN = Literal(")").makeStripped()
+let VALUE_LIST = oneOrMore((cgFloatValue + OPT_COMMA).makeStripped().makeFlattened())
+
+// TODO: Should set manual min and max value instead of relying on 0..<infinite VALUE_LIST
+
+let matrix = (Literal("matrix") + LPAREN + VALUE_LIST + RPAREN)
+let translate = (Literal("translate") + LPAREN + VALUE_LIST + RPAREN)
+let scale = (Literal("scale") + LPAREN + VALUE_LIST + RPAREN)
+let rotate = (Literal("rotate") + LPAREN + VALUE_LIST + RPAREN)
+let skewX = (Literal("skewX") + LPAREN + VALUE_LIST + RPAREN)
+let skewY = (Literal("skewY") + LPAREN + VALUE_LIST + RPAREN)
+let transform = (matrix | translate | scale | rotate | skewX | skewY).makeFlattened()
+let transforms = oneOrMore((transform + OPT_COMMA).makeFlattened())
+
 
 class SwiftParsingTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+        let result = transforms.parse("translate(0,0)")
+        if let value = result.value as? [Any] {
+            if let value = value[0] as? [Any] {
+                let type = value[0] as? String
+                XCTAssertEqual(type!, "translate")
+                // TODO now values
+            }
         }
     }
-    
 }
+
